cb933687cd5079470527176c3de56f9b2d61047e
RoyzellW <royzellwalton@gmail.com>
2018-11-09 07:42:59 -0600

    Alpha

---
+++ royzellw/JavaScript.js
@@ -0,0 +1,23 @@
+
+/* animate smooth scrolling sections */
+$("nav div ul li a").on('click', function(e) {
+
+    // prevent default anchor click behavior
+    e.preventDefault();
+
+    // store hash
+    var hash = this.hash;
+
+    // animate
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 1000, function(){
+
+        // when done, add hash to url
+        // (default click behaviour)
+        window.location.hash = hash;
+    });
+
+});
+
+// AOS.init();
